d29ad9c455f5593ae756e02b9fd58b1bac02439a
amichalo421 <andre.michalowski@gmail.com>
2019-11-19 00:13:29 +0100

    Logo code added

---
+++ src/components/AFLogo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Logo from './logoFile.png';
+
+export default function AFLogoFxn() {
+    return(
+        <img src={Logo} alt="Brand Logo for Anywhere Fitness" />
+    )
+}
