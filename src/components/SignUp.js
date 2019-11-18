4f9c71db8364d5f054f2f5f043a482e3c2ed0e33
amichalo421 <andre.michalowski@gmail.com>
2019-11-18 15:30:37 +0100

    Submit_styling_added

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -6,73 +6,20 @@ export default function SignUp(props) {
         <div className="SignUp-Container">
             <Form>
                 <FormGroup>
-                    <Label for="exampleEmail">Email</Label>
-                    <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+                    <Label for="exampleEmail">Email:</Label>
+                    <Input type="email" name="email" id="exampleEmail" placeholder="JonDoe@gmail.com" />
                 </FormGroup>
                 <FormGroup>
-                    <Label for="examplePassword">Password</Label>
-                    <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+                    <Label for="examplePassword">Password:</Label>
+                    <Input type="password" name="password" id="examplePassword" placeholder="$PAssword124" />
                 </FormGroup>
                 <FormGroup>
-                    <Label for="exampleSelect">Select</Label>
-                    <Input type="select" name="select" id="exampleSelect">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                    </Input>
+                    <Label for="instructorCode">instructorCode:</Label>
+                    <Input type="instructorCode" name="instructorCode" id="instructorCode" placeholder="(Instructors Only)" />
                 </FormGroup>
-                <FormGroup>
-                    <Label for="exampleSelectMulti">Select Multiple</Label>
-                    <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                    </Input>
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleText">Text Area</Label>
-                    <Input type="textarea" name="text" id="exampleText" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleFile">File</Label>
-                    <Input type="file" name="file" id="exampleFile" />
-                    <FormText color="muted">
-                    This is some placeholder block-level help text for the above input.
-                    It's a bit lighter and easily wraps to a new line.
-                    </FormText>
-                </FormGroup>
-                <FormGroup tag="fieldset">
-                    <legend>Radio Buttons</legend>
-                    <FormGroup check>
-                    <Label check>
-                        <Input type="radio" name="radio1" />{' '}
-                        Option one is this and that—be sure to include why it's great
-                    </Label>
-                    </FormGroup>
-                    <FormGroup check>
-                    <Label check>
-                        <Input type="radio" name="radio1" />{' '}
-                        Option two can be something else and selecting it will deselect option one
-                    </Label>
-                    </FormGroup>
-                    <FormGroup check disabled>
-                    <Label check>
-                        <Input type="radio" name="radio1" disabled />{' '}
-                        Option three is disabled
-                    </Label>
-                    </FormGroup>
-                </FormGroup>
-                <FormGroup check>
-                    <Label check>
-                    <Input type="checkbox" />{' '}
-                    Check me out
-                    </Label>
-                </FormGroup>
-                <Button>Submit</Button>
+                <div className="text-right">
+                    <Button>Submit</Button>
+                </div>
             </Form>
         </div>
     )
