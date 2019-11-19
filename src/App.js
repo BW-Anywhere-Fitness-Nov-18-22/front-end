b975b569c52c91a8ba5b3745a68b214fd0b21a77
John Afolabi <johnafolabi.da@gmail.com>
2019-11-19 18:43:29 +0100

    Add routes for Client and Instructor dashboards

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Navigation from "./components/Header";
 import { Route } from "react-router-dom";
 import LoginWFormik from "./components/Login";
 import SignUpWFormik from "./components/SignUp";
+import ClientDashboard from "./components/dashboard/Client";
+import InstructorDashboard from "./components/dashboard/Instructor";
 
 function App() {
   return (
@@ -21,6 +23,20 @@ function App() {
         path="/register"
         render={props => {
           return <SignUpWFormik {...props} />;
+        }}
+      />
+
+      <Route
+        path="/dashboard/client"
+        render={props => {
+          return <ClientDashboard {...props} />;
+        }}
+      />
+
+      <Route
+        path="/dashboard/instructor"
+        render={props => {
+          return <InstructorDashboard {...props} />;
         }}
       />
     </div>
