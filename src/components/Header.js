3316956b9a52f83f3d1b9d1d443ab2d909fddafa
amichalo421 <andre.michalowski@gmail.com>
2019-11-19 18:12:11 +0100

    Removed notes from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -73,24 +73,3 @@ export default function Navigation() {
 
 
 
-
-// export function Navigation() {
-//     return (
-//         <nav class="navbar navbar-default">
-//             <div class="container-fluid">
-//                 <div class="navbar-header">
-//                     <NavLink class="navbar-brand" exact to={'/'}>ANYWHERE FITNESS</NavLink>
-//                 </div>
-//                 <ul class="nav navbar-nav">
-//                     <li><NavLink exact to={'/'}>Home</NavLink></li>
-//                     <li><NavLink to={'/classes'}>Classes</NavLink></li>
-//                     <li><NavLink to={'/Sign-Up'}>Sign-Up</NavLink></li>
-//                     <li><NavLink to={'/Login'}>Login</NavLink></li>
-//                 </ul>
-//             </div>
-//         </nav>
-//     )
-// }
-
-
-
