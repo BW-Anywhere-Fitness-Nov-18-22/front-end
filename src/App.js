6f30b100e0f83b48caa01a513f45b6324508cc08
John Afolabi <johnafolabi.da@gmail.com>
2019-11-19 18:37:14 +0100

    Route and render components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navigation from "./components/Header";
+import { Route } from "react-router-dom";
+import LoginWFormik from "./components/Login";
+import SignUpWFormik from "./components/SignUp";
 
 function App() {
   return (
     <div>
+      <Navigation />
+
+      <Route
+        path="/login"
+        render={props => {
+          return <LoginWFormik {...props} />;
+        }}
+      />
+
+      <Route
+        path="/register"
+        render={props => {
+          return <SignUpWFormik {...props} />;
+        }}
+      />
     </div>
   );
 }
 
 export default App;
-
- 
