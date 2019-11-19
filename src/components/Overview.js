2da751d4410d7f73aa55ebf43a90592049fb0dcb
amichalo421 <andre.michalowski@gmail.com>
2019-11-19 01:25:19 +0100

    Overview component and content added

---
+++ src/components/Overview.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+
+export default function Overview(props) {
+    return (
+        <div className="SignUp-Container">
+                <div className="text-right">
+                    <Button>Skip</Button>
+                </div>
+                <div>
+                    Filler text that will eventually be some kind of overview or waiver. 
+                    Filler text that will eventually be some kind of overview or waiver.
+                    Filler text that will eventually be some kind of overview or waiver.
+                    Filler text that will eventually be some kind of overview or waiver.
+                    Filler text that will eventually be some kind of overview or waiver.
+                    Filler text that will eventually be some kind of overview or waiver.
+                    Filler text that will eventually be some kind of overview or waiver.
+                    Filler text that will eventually be some kind of overview or waiver.
+                    Filler text that will eventually be some kind of overview or waiver.
+                    Filler text that will eventually be some kind of overview or waiver.
+                    Filler text that will eventually be some kind of overview or waiver.
+                    Filler text that will eventually be some kind of overview or waiver.
+                    Filler text that will eventually be some kind of overview or waiver.
+                    Filler text that will eventually be some kind of overview or waiver.
+                    Filler text that will eventually be some kind of overview or waiver.
+                    Filler text that will eventually be some kind of overview or waiver.
+                    Filler text that will eventually be some kind of overview or waiver.
+                    Filler text that will eventually be some kind of overview or waiver.
+                    Filler text that will eventually be some kind of overview or waiver.
+                    Filler text that will eventually be some kind of overview or waiver.
+                    Filler text that will eventually be some kind of overview or waiver.
+                    
+                </div>
+                <div className="text-right">
+                    <Button>Confirm</Button>
+                </div>
+        </div>
+    )
+}
