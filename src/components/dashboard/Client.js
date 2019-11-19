48febba7170af79452dd8d5eeb89c4e563863a0d
John Afolabi <johnafolabi.da@gmail.com>
2019-11-19 18:43:59 +0100

    Build Client and Instructor Dashboard

---
+++ src/components/dashboard/Client.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ClientClassCardContainer from "../client/ClientClassCardContainer";
+
+function ClientDashboard() {
+  return <ClientClassCardContainer />;
+}
+
+export default ClientDashboard;
