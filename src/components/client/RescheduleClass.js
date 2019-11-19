39196b6a22dd1117b1c60729170d0c0aacebac9b
John Afolabi <johnafolabi.da@gmail.com>
2019-11-19 11:28:17 +0100

    Change Input format

--- src/components/client/RescheduleClass.js
+++ src/components/client/RescheduleClass.js
@@ -5,22 +5,8 @@ function RescheduleClass() {
   return (
     <Form>
       <FormGroup>
-        <Label for="exampleDate">Date</Label>
-        <Input
-          type="date"
-          name="date"
-          id="exampleDate"
-          placeholder="date placeholder"
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleTime">Time</Label>
-        <Input
-          type="time"
-          name="time"
-          id="exampleTime"
-          placeholder="time placeholder"
-        />
+        <Label for="datetime">New Time</Label>
+        <Input type="datetime-local" name="datetime" id="datetime" />
       </FormGroup>
       <Button color="success">Submit</Button>
     </Form>
